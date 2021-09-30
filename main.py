778105ccf4b150bd43f31d29b4e58ef98aebbe00
Guilherme Soeiro de Carvalho Caporali <55262460+guilherme13c@users.noreply.github.com>
2021-09-30 20:36:45 -0300

    Update 1.1v
    Tracing added.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame, os, sys
 import random
+import math
 from pygame.locals import *
 
 class Point:
@@ -9,6 +10,7 @@ class Point:
     self.velocity = [0, 0]
     self.pos = [x, y]
     self.charge = charge
+    self.trace = []
 
   def draw(self):
     pygame.draw.circle(screen, self.color, self.pos, self.radius)
@@ -21,7 +23,7 @@ os.environ['SDL_VIDEO_WINDOW_POS'] = '1'
 #Definition of screen settings
 WIDTH = 1200
 HEIGHT = 1000
-screen = pygame.display.set_mode([WIDTH, HEIGHT], DOUBLEBUF)
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
 screen.set_alpha(None)
 
 #Definition of colors
@@ -32,13 +34,14 @@ RED = pygame.Color(255,0,0)
 GREEN = pygame.Color(0,255,0)
 BLUE = pygame.Color(0,0,255)
 
-FPS = 100
+FPS = 60
 clock = pygame.time.Clock()
 
 screen.fill(GREY)
 points = []
 
-K=0.6
+K = 2
+MASS = 1
 MAX_VELOCITY = FPS - 10
 
 #Needed functions
@@ -51,7 +54,7 @@ def calculateVectorNorm(vect:list):
 def calculateElectricForceValue(p1:Point, p2:Point):
   d = calculateVectorNorm(calculateDisplacementVector(p1,p2))
   if d == 0:
-    d = 0.001
+    d = 0.0001
 
   return (K*p1.charge*p2.charge)/(d**2)
 
@@ -60,18 +63,25 @@ while True:
   screen.fill(GREY)
 
   for point1 in points:
+    point1.trace.append([int(point1.pos[0]), int(point1.pos[1])])
+    if len(point1.trace) > 100:
+      point1.trace.pop(0)
+
     if (point1.pos[0]+point1.velocity[0] > WIDTH) or (point1.pos[0]+point1.velocity[0] < 0):
-      point1.velocity[0] = -point1.velocity[0]
+      point1.velocity[0] = -0.8*point1.velocity[0]
     if (point1.pos[1]+point1.velocity[1] > HEIGHT) or (point1.pos[1]+point1.velocity[1] < 0):
-      point1.velocity[1] = -point1.velocity[1]
+      point1.velocity[1] = -0.8*point1.velocity[1]
 
     point1.pos[0] += point1.velocity[0]
     point1.pos[1] += point1.velocity[1]
+
     if point1.velocity[0] > MAX_VELOCITY:
       point1.velocity[0] = MAX_VELOCITY
     if point1.velocity[1] > MAX_VELOCITY:
       point1.velocity[1] = MAX_VELOCITY
 
+    if len(point1.trace) >= 2:
+      pygame.draw.lines(screen, point1.color, False, point1.trace, 1)
     point1.draw()
 
     for point2 in points:
@@ -84,8 +94,8 @@ while True:
 
         directionNormalized = direction[0]/directionNorm, direction[1]/directionNorm
 
-        point1.velocity[0] += directionNormalized[0]*forceVal
-        point1.velocity[1] += directionNormalized[1]*forceVal
+        point1.velocity[0] += directionNormalized[0]*forceVal/MASS
+        point1.velocity[1] += directionNormalized[1]*forceVal/MASS
 
   for event in pygame.event.get():
     if (event.type == QUIT):
