86b0d6db9572e947051d70bff30bf95b5e29ed8d
Guilherme Soeiro de Carvalho Caporali <55262460+guilherme13c@users.noreply.github.com>
2021-11-06 19:06:08 -0300

    Feartures added/improved
    There are now two different ways of creating new particles. The old way, using the mouse buttons now creates the particle at the cursor's position. The new way generates them with random charge (positive or negative) and random position.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame, os, sys
 import random
 from pygame.locals import *
 
+#Definition of classes
 class Point:
   def __init__(self, color:pygame.Color, x:int, y:int, charge:int):
     self.color = color
@@ -11,6 +12,7 @@ class Point:
     self.charge = charge
     self.trace = []
 
+  #Definition of methods
   def draw(self):
     pygame.draw.circle(screen, self.color, self.pos, self.radius)
 
@@ -40,21 +42,20 @@ points = []
 
 K = 2
 MASS = 1
-MAX_VELOCITY = FPS - 10
+MAX_VELOCITY = FPS * 0.8
 
-#Needed functions
+#Needed Liner Algebra functions
 def calculateDisplacementVector(p1:Point, p2:Point):
   return [p1.pos[0]-p2.pos[0], p1.pos[1]-p2.pos[1]]
 
 def calculateVectorNorm(vect:list):
   r = (vect[0]**2+vect[1]**2)**(1/2)
-  if r == 0:
-    return 0.0001
   return r
 
 def calculateElectricForceValue(p1:Point, p2:Point):
   d = calculateVectorNorm(calculateDisplacementVector(p1,p2))
-  if d == 0:
+
+  if d == 0:    #this conditional avoids division by zero errors
     d = 0.0001
 
   return (K*p1.charge*p2.charge)/(d**2)
@@ -63,6 +64,7 @@ def calculateElectricForceValue(p1:Point, p2:Point):
 while True:
   screen.fill(GREY)
 
+  #store and draw trace
   for point1 in points:
     point1.trace.append([int(point1.pos[0]), int(point1.pos[1])])
     if len(point1.trace) > 100:
@@ -85,6 +87,7 @@ while True:
       pygame.draw.lines(screen, point1.color, False, point1.trace, 1)
     point1.draw()
 
+    #calculate and update velocity
     for point2 in points:
       if point2 != point1:
         forceVal = calculateElectricForceValue(point1, point2)
@@ -98,6 +101,7 @@ while True:
         point1.velocity[0] += directionNormalized[0]*forceVal/MASS
         point1.velocity[1] += directionNormalized[1]*forceVal/MASS
 
+        #impact detection and trajectory update
         if (calculateVectorNorm(calculateDisplacementVector(point1, point2))-point1.radius-point2.radius <= 0):
           impactDirection = [0, 0]
           impactDirection[0] = calculateDisplacementVector(point1, point2)[0]/calculateVectorNorm(calculateDisplacementVector(point1, point2))
@@ -109,6 +113,7 @@ while True:
           point2.velocity[0] *= 0.9*impactDirection[0]
           point2.velocity[1] *= 0.9*impactDirection[1]
 
+  #Input detection/receiving
   for event in pygame.event.get():
     if (event.type == QUIT):
       pygame.quit()
@@ -117,14 +122,22 @@ while True:
       if (event.key == pygame.K_ESCAPE):
         pygame.quit()
         sys.exit()
+      elif (event.key == pygame.K_SPACE):
+        seed = random.random()
+        if (seed > 0.5):
+          newPoint = Point(RED , random.randint(0, WIDTH), random.randint(0, HEIGHT), +10)
+        else:
+          newPoint = Point(BLUE , random.randint(0, WIDTH), random.randint(0, HEIGHT), -10)
+        points.append(newPoint)
     else:
       mouse_button = pygame.mouse.get_pressed()
       if (mouse_button[0]):
-        newPoint = Point(BLUE, random.randint(0, WIDTH), random.randint(0, HEIGHT), -10)
+        newPoint = Point(BLUE, pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1], -10)
         points.append(newPoint)
       if (mouse_button[2]):
-        newPoint = Point(RED , random.randint(0, WIDTH), random.randint(0, HEIGHT), +10)
-        points.append(newPoint)
+        newPoint = Point(RED , pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1], +10)
+        points.append(newPoint)  
 
+  #screen updating
   pygame.display.update()
   clock.tick(FPS)
